3d5aec757e935fa2926ecfef93978daa35069e34
Abolfazl Mikaniki <abolfazlmikaniki6@gmail.com>
2023-11-13 08:58:05 +0330

    Building search input

--- src/components/Navigarion/Nav.tsx
+++ src/components/Navigarion/Nav.tsx
@@ -1,12 +1,14 @@
 import {HStack, Image ,Text} from '@chakra-ui/react'
 import logo from '../../assets/Logo/logo.webp'
 import Switch_Mode from '../ColorModeSwitch/ModeSwitch'
+import SearchInput from '../SearchInput/SearchInput'
 function NavBar() {
   
 return(
 
     <HStack justifyContent='space-between' padding='0px 20px'>
         <Image src={logo} boxSize='60px'></Image>
+        <SearchInput/>
         <Switch_Mode />
     </HStack>
 
---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,15 @@
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
+import { BsSearch } from "react-icons/bs"
+
+const  SearchInput=():JSX.Element=>{
+
+return(
+    <InputGroup>
+        <InputLeftElement children={<BsSearch/>}/>
+        <Input borderRadius={20} placeholder="Search game..." variant='filled'/>
+    </InputGroup>
+ )
+  
+}
+
+export default SearchInput
